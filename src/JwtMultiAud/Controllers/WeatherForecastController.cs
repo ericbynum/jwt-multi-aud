6529add6e70450dfd5370105ea8cf56e178e16e7
Eric Bynum <bynum.eric@gmail.com>
2022-01-02 14:53:40 -0600

    feat: require authorization for controller

--- src/JwtMultiAud/Controllers/WeatherForecastController.cs
+++ src/JwtMultiAud/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwtMultiAud.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
