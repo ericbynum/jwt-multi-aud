837032b36d37e7c5adcc4464fac9af8920342382
Eric Bynum <bynum.eric@gmail.com>
2022-01-02 14:52:10 -0600

    refactor: move jwt claims to appsettings

--- src/JwtMultiAud/Controllers/AuthController.cs
+++ src/JwtMultiAud/Controllers/AuthController.cs
@@ -10,29 +10,38 @@ namespace JwtMultiAud.Controllers;
 [ApiController]
 public class AuthController : ControllerBase
 {
+    private readonly IConfiguration configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
     [AllowAnonymous]
     [HttpPost("auth")]
     public IActionResult Authenticate()
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Guid.NewGuid().ToString();
+        var key = configuration.GetValue<string>("JwtSecurityKey");
         var tokenKey = Encoding.ASCII.GetBytes(key);
+
+        var claims = new List<Claim>();
+        var audiences = configuration.GetSection("JwtAudiences").Get<string[]>();
+        foreach (var audience in audiences)
+        {
+            // adding multiple "aud" claim keys will become a single "aud" claim with an array of values
+            claims.Add(new Claim("aud", audience));
+        }
 
         var tokenDescriptor = new SecurityTokenDescriptor()
         {
-            Issuer = "ebynum",
-            Subject = new ClaimsIdentity(
-                new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, "eric"),
-                    new Claim("aud", "aud1"),
-                    new Claim("aud", "aud2"),
-                }),
+            Issuer = configuration.GetValue<string>("JwtIssuer"),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(1),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
         };
-        
+
         var token = tokenHandler.CreateToken(tokenDescriptor);
         var jwt = tokenHandler.WriteToken(token);
 
